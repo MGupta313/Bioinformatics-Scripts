7cb6e60dc334a71b6a59f4df668dc86601e0c832
MGupta313 <54957102+MGupta313@users.noreply.github.com>
2020-04-05 19:47:29 -0400

    Update overlapBED

--- overlapBED.py
+++ overlapBED.py
@@ -1,88 +1,142 @@
 #!/usr/bin/env python3
-
-
+#
+# Your code here
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("-i1",help="Enter the first file:")
-parser.add_argument("-i2",help="Enter the second file:")
-parser.add_argument("-m",type=float,help="Enter minimum percent overlap:")
+parser.add_argument("-i1",help="enter the first file")
+parser.add_argument("-i2",help="enter the second file")
+parser.add_argument("-m",type=float,help="enter minimum percent overlap")
 parser.add_argument("-j", action="store_true",
                     help="allows to print member of the first set and the member of the second set that it overlaps with on the same line")
 parser.add_argument("-o",help="name of output file")
 args = parser.parse_args()
 
-linesFile1 = []
-linesFile2 = []
-threshold = 0
-with open(args.i1, 'r') as file1:
-    for lines in file1:
-        lines = lines.split()
-        linesFile1.append(lines)
-# print(linesFile1, "\n")
-with open(args.i2, 'r') as file2:
-    for lines in file2:
-        lines = lines.rstrip("\t")
-        linesFile2.append(lines.split())
-# print(linesFile2)
-with open(args.o,'w') as outputOverlapFile:
-    if args.j:
-        for i in range(len(linesFile1)):
-            for j in range(len(linesFile2)):
-                if linesFile1[i][0] == linesFile2[j][0]: #making sure that comparison is done on the same chromosome
-                    if linesFile1[i][1] > linesFile2[j][1]: #checking if start_one > start_two
-                        if linesFile1[i][2] < linesFile2[j][2]: #checking if stop_one < stop_two
-                            overlap = min(int(linesFile1[i][2]), int(linesFile2[j][2])) - max(int(linesFile1[i][1]), int(linesFile2[j][1]))
-                            if overlap >= threshold:
-                                outputOverlapFile.write(linesFile1[i])
-                                outputOverlapFile.write("\t")
-                                outputOverlapFile.write(linesFile2[j])
-                        elif linesFile1[i][2] == linesFile2[j][2]: #checking if stop_one < stop_two
-                            overlap = min(int(linesFile1[i][2]), int(linesFile2[j][2])) - max(int(linesFile1[i][1]), int(linesFile2[j][1]))
-                            if overlap >= threshold:
-                                outputOverlapFile.write(linesFile1[i])                                 
-                                outputOverlapFile.write("\t")                                 
-                                outputOverlapFile.write(linesFile2[j])
-                        elif linesFile1[i][2] > linesFile2[j][2]: #checking if stop_one < stop_two
-                            overlap = min(int(linesFile1[i][2]), int(linesFile2[j][2])) - max(int(linesFile1[i][1]), int(linesFile2[j][1]))
-                            if overlap >= threshold:
-                                outputOverlapFile.write(linesFile1[i])                                 
-                                outputOverlapFile.write("\t")                                 
-                                outputOverlapFile.write(linesFile2[j])
-                    elif linesFile1[i][1] == linesFile2[j][1]: #checking if start_one > start_two
-                        if linesFile1[i][2] < linesFile2[j][2]: #checking if stop_one < stop_two
-                            overlap = min(int(linesFile1[i][2]), int(linesFile2[j][2])) - max(int(linesFile1[i][1]), int(linesFile2[j][1]))
-                            if overlap >= threshold:
-                                outputOverlapFile.write(linesFile1[i])                                 
-                                outputOverlapFile.write("\t")                                 
-                                outputOverlapFile.write(linesFile2[j])
-                        elif linesFile1[i][2] == linesFile2[j][2]: #checking if stop_one < stop_two
-                            overlap = min(int(linesFile1[i][2]), int(linesFile2[j][2])) - max(int(linesFile1[i][1]), int(linesFile2[j][1]))
-                            if overlap >= threshold:
-                                outputOverlapFile.write(linesFile1[i])                                 
-                                outputOverlapFile.write("\t")                                 
-                                outputOverlapFile.write(linesFile2[j])
-                        elif linesFile1[i][2] > linesFile2[j][2]: #checking if stop_one < stop_two
-                            overlap = min(int(linesFile1[i][2]), int(linesFile2[j][2])) - max(int(linesFile1[i][1]), int(linesFile2[j][1]))
-                            if overlap >= threshold:
-                                outputOverlapFile.write(linesFile1[i])                                 
-                                outputOverlapFile.write("\t")                                 
-                                outputOverlapFile.write(linesFile2[j])
-                    elif linesFile1[i][1] < linesFile2[j][1]: #checking if start_one > start_two
-                        if linesFile1[i][2] < linesFile2[j][2]: #checking if stop_one < stop_two
-                            overlap = min(int(linesFile1[i][2]), int(linesFile2[j][2])) - max(int(linesFile1[i][1]), int(linesFile2[j][1]))
-                            if overlap >= threshold:
-                                outputOverlapFile.write(linesFile1[i])                                 
-                                outputOverlapFile.write("\t")                                 
-                                outputOverlapFile.write(linesFile2[j])
-                        elif linesFile1[i][2] == linesFile2[j][2]: #checking if stop_one < stop_two
-                            overlap = min(int(linesFile1[i][2]), int(linesFile2[j][2])) - max(int(linesFile1[i][1]), int(linesFile2[j][1]))
-                            if overlap >= threshold:
-                                outputOverlapFile.write(linesFile1[i])                                 
-                                outputOverlapFile.write("\t")                                 
-                                outputOverlapFile.write(linesFile2[j])
-                        elif linesFile1[i][2] > linesFile2[j][2]: #checking if stop_one < stop_two
-                            overlap = min(int(linesFile1[i][2]), int(linesFile2[j][2])) - max(int(linesFile1[i][1]), int(linesFile2[j][1]))
-                            if overlap >= threshold:
-                                outputOverlapFile.write(linesFile1[i])                                 
-                                outputOverlapFile.write("\t")                                 
-                                outputOverlapFile.write(linesFile2[j])
+chromosome_file_one=[]
+fragments_file_one=[]
+chromosome_fragment=[]
+with open(args.i1,'r') as file_one:
+    for line in file_one:
+        line=line.rstrip()
+        line=line.split("\t")
+        if line[0] not in chromosome_file_one:
+            chromosome_file_one.append(line[0])
+            if len(chromosome_fragment)!=0:
+                fragments_file_one.append(chromosome_fragment)
+                chromosome_fragment=[]
+        chromosome_fragment.append(line)
+    fragments_file_one.append(chromosome_fragment)
+
+
+    
+chromosome_file_two=[]
+fragments_file_two=[]
+chromosome_fragment=[]
+with open(args.i2,'r') as file_two:
+    for line in file_two:
+        line=line.rstrip()
+        line=line.split("\t")
+        if line[0] not in chromosome_file_two: 
+            chromosome_file_two.append(line[0])
+            if len(chromosome_fragment)!=0:
+                fragments_file_two.append(chromosome_fragment)
+                chromosome_fragment=[]
+        chromosome_fragment.append(line)
+    fragments_file_two.append(chromosome_fragment)
+        
+duplicate_output_list=[]
+with open(args.o,'w') as output_file:
+        t=0 
+        fragment_overlap=[]
+        for j in range(len(fragments_file_one[i])):
+            print("at {}th fragment of {}th chromosome in file one".format(j,i))
+            flag_overlap=0
+            flag_t=0
+            for k in range(t,len(fragments_file_two[i])):
+                print("comparing {}th fragment of file one with {}th fragment of {}th chromosome in file two".format(j,k,i))
+
+                if int(fragments_file_one[i][j][1])>int(fragments_file_two[i][k][2]):
+                    continue
+                else:
+                    minimum=min(int(fragments_file_one[i][j][2]),int(fragments_file_two[i][k][2]))
+                    #print(int(fragments_file_one[i][j][2]))
+                    #print(int(fragments_file_two[i][k][2]))
+                    #print(minimum)
+                    maximum=max(int(fragments_file_one[i][j][1]),int(fragments_file_two[i][k][1]))
+                    #print(int(fragments_file_one[i][j][1]))
+                    #print(int(fragments_file_two[i][k][1]))
+                    #print(maximum)
+                    overlap=minimum-maximum
+                    if flag_t!=1:
+                        t=k
+                        flag_t=1
+                    if overlap<=0:
+                        break
+                    else:
+                        print(overlap)
+                        length=int(fragments_file_one[i][j][2])-int(fragments_file_one[i][j][1])
+                        print(length)
+                        percent_overlap=(overlap/length)*100
+                        print((percent_overlap))
+                        if percent_overlap>=args.m:
+                            if args.j:
+                                with open(args.o,'a') as output_file:
+                                    output_file.write(fragments_file_one[i][j][0])
+                                    output_file.write("\t")
+                                    output_file.write(fragments_file_one[i][j][1])
+                                    output_file.write("\t")
+                                    output_file.write(fragments_file_one[i][j][2])
+                                    output_file.write("\t\t")
+                                    output_file.write(fragments_file_two[i][k][0])
+                                    output_file.write("\t")
+                                    output_file.write(fragments_file_two[i][k][1])
+                                    output_file.write("\t")
+                                    output_file.write(fragments_file_two[i][k][2])
+                                    output_file.write("\n")
+                            else:
+                                duplicate_output_list.append(fragments_file_one[i][j][0]+fragments_file_one[i][j][1]+fragments_file_one[i][j][2])
+
+                                    
+unique_output_list=[]
+if not args.j:
+    for unique in duplicate_output_list:
+        if unique not in unique_output_list:
+            unique_output_list.append(unique)
+
+
+                    
+                    
+
+                    
+
+                    
+            
+            
+                
+
+    
+    
+        
+
+    
+        
+
+
+
+
+
+
+
+                                    
+                                    
+                                    
+
+                                    
+
+
+
+
+
+
+
+
+
