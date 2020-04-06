e92fc63ab4d39dc9c679135af76174d2f5a368e1
MGupta313 <54957102+MGupta313@users.noreply.github.com>
2020-04-05 20:52:01 -0400

    Update kmer-counter

--- kmer-counter.py
+++ kmer-counter.py
@@ -1,30 +1,32 @@
-#! /usr/bin/python3
+#!/bin/env python
 
+# Write your code here
 import sys
 
-#taking inputs
-k=int(sys.argv[1])
-filename=sys.argv[2]
+with open(sys.argv[2]) as f:
+    next(f) #to skip first line
+    lines = f.readlines()
+y = ''.join(lines)
+final = y.replace('\n', '')
 
-#parsing the input file
-file=open(filename,'r')
-file.readline()
+kmerDict = {}
+kmerLength=sys.argv[1]
+rangeOfIndex = len(final)-kmerLength
+count = 0
+for i in range(rangeOfIndex+1):
+#     kmerLength=3 #arg
+    kmerSeq = final[i:i+kmerLength]
+#     print(kmer)
+    if kmerSeq in kmerDict:
+        kmerDict[kmerSeq] +=1
+    else:
+        kmerDict[kmerSeq] = 1
+# print(kmerDict)
 
-with open(filename) as file
-    seqList = []
-    for line in file:
-        line=line.rstrip()
-        seqList+=line
+keyList = kmerDict.keys()
+# print(keyList)
+sortedKeyList = sorted(keyList)
+# print(a)
 
-    l=len(seqList)
-    kmerDict = {}
-    for i in range(l-k):
-        key=seqList[i:i+k]
-        key="".join(key)
-        if key in kmerDict:
-            kmerDict[key] += 1
-        else:		
-            kmerDict[key] = 1	
-
-    for key in sorted(kmerDict):
-        print(key,"\t",kmerDict[key])
+for key in sortedKeyList:
+    print(key,"\t",kmerDict[key])
